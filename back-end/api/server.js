aff3aa6ad32e8bb77f56d52a867e2f9ca16936be
Mauro Leal <mauro.costa.12.j@hotmail.com>
2025-02-22 22:05:43 -0300

    Ajustes Para o deploy

--- back-end/api/server.js
+++ back-end/api/server.js
@@ -4,9 +4,12 @@
 // Endpoint
 // Middleware
 
-import express from "express";
+import express, { request, response } from "express";
 import cors from "cors";
 import { db } from "./connect.js";
+import path, { dirname } from "path";
+
+const __dirname = path.resolve();
 
 const app = express();
 const PORT = 3001;
@@ -14,17 +17,23 @@ const PORT = 3001;
 app.use(cors());
 // app.use(express.json());
 
-app.get("/", (request, response) => {
+app.get("/api/", (request, response) => {
   response.send("Só vamos trabalhar com os endpoints '/artists' e '/songs'");
 });
 
-app.get("/artists", async (request, response) => {
+app.get("/api/artists", async (request, response) => {
   response.send(await db.collection("artists").find({}).toArray());
 });
 
-app.get("/songs", async (request, response) => {
+app.get("/api/songs", async (request, response) => {
   response.send(await db.collection("songs").find({}).toArray());
 });
+
+app.use(express.static(path.join(__dirname,"../../front-end/dist")))
+
+app.get("*", (request, response) => {
+  response.sendFile(path.join(__dirname,"../../front-end/dist/index.html"))
+})
 
 app.listen(PORT, () => {
   console.log(`Servidor está escutando na porta ${PORT}`);
